415e07122a4daae712450e6562c76545f11b614e
jask1m <jkim94960@gmail.com>
2024-06-18 14:37:57 -0700

    lab5

--- week3/Lab5.java
+++ week3/Lab5.java
@@ -2,10 +2,43 @@ import java.util.* ;
 
 public class Lab5
 {
-	public static void main(String[] args)  {
-		// Add you tests here
-	
-	}
+	public static void main(String[] args) {
+		// tests go here
+		Integer[] testArray = {100, 12, 7, 120, 4, 14, 66, 5, 3};
+		// Sort the array
+		Arrays.sort(testArray);
+
+		System.out.println("Sorted array: " + Arrays.toString(testArray));
+		System.out.println("Running tests for inArrayIterativeSorted:");
+
+		System.out.println("Expected output for 14: true");
+		System.out.println("Actual output for 14: " + inArrayIterativeSorted(testArray, 14));
+
+		System.out.println("Expected output for 66: true");
+		System.out.println("Actual output for 66: " + inArrayIterativeSorted(testArray, 66));
+
+		System.out.println("Expected output for 13: false");
+		System.out.println("Actual output for 13: " + inArrayIterativeSorted(testArray, 13));
+
+		System.out.println("Expected output for 100: true");
+		System.out.println("Actual output for 100: " + inArrayIterativeSorted(testArray, 100));
+
+		System.out.println("Expected output for 1: false");
+		System.out.println("Actual output for 1: " + inArrayIterativeSorted(testArray, 1));
+
+		System.out.println("Running tests for modifiedSelectionSort:");
+
+		Integer[] arr1 = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 };
+    modifiedSelectionSort(arr1, 0);
+    System.out.println("Expected output for arr1: [1, 1, 2, 3, 3, 4, 5, 5, 6, 9]");
+    System.out.println("Actual output for arr1: " + Arrays.toString(arr1));
+
+    // Test Case 2: String array
+    String[] arr2 = { "banana", "apple", "orange", "pear", "grape" };
+    modifiedSelectionSort(arr2, 0);
+    System.out.println("Expected output for arr2: [apple, banana, grape, orange, pear]");
+    System.out.println("Actual output for arr2: " + Arrays.toString(arr2));
+}
 
 	// Problem 1
 	public static <T extends Comparable<? super T>> boolean inArrayIterativeSorted(T[] anArray, T anEntry) {
@@ -17,10 +50,10 @@ public class Lab5
 					return true;
 				} 
 				else if (anArray[mid].compareTo(anEntry) > 0) {
-					first = mid +1;
+					last = mid -1;
 				}
 				else {
-					last = mid -1;
+					first = mid +1;
 				}
 		}
 		return false;
@@ -35,10 +68,10 @@ public class Lab5
 				int largest = i;
 
 				for (int j = i + 1; j <= last_index; j++) {
-					if (a[j].compareTo(a[i]) < 0) {
+					if (a[j].compareTo(a[smallest]) < 0) {
 						smallest = j;
 					}
-					else if (a[j].compareTo(a[i]) > 0) {
+					else if (a[j].compareTo(a[largest]) > 0) {
 						largest = j;
 					}
 				}
@@ -63,5 +96,11 @@ public class Lab5
 			}
     } 
     // For problem 2, how many comparisons are necessary to sort n values?  Write your answer below
-		//  
+		// In this sorting method, there are two for loops. The outer for loop iterates n/2 times, considering that n is the length.
+		// The inner loop is where the comparisons are being made.
+		// there are n-i, and n-i comparisons to find the smallest and largest element respectfully.
+		// the total comparisons in the inner loop is 2(n-i)
+		// the inner loop comparisons can be considered as O(n)
+		// since the outer loop iterates n/2 times, the total comparisons can be O(n) * O(n/2)
+		// this gives a total of O(n^2 / 2) comparisons, or more simply, O(n^2)
 } 
